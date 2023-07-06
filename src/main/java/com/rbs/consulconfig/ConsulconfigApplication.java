1dbd145b1303100d370d74b3f6d996ebc0cc778a
Navin3d <71096790+Navin3d@users.noreply.github.com>
2023-07-06 20:20:43 +0530

    Update ConsulconfigApplication.java

--- src/main/java/com/rbs/consulconfig/ConsulconfigApplication.java
+++ src/main/java/com/rbs/consulconfig/ConsulconfigApplication.java
@@ -1,13 +1,33 @@
 package com.rbs.consulconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rbs.consulconfig.services.GitService;
+
 @SpringBootApplication
-public class ConsulconfigApplication {
+public class ConsulconfigApplication implements CommandLineRunner {
+	
+	@Value("${git.repo.url}")
+    	private String gitRepoUrl;
+	
+	@Value("${git.local.repo}")
+	private String localRepoPath;
+	
+	@Autowired
+	private GitService gitService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsulconfigApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		gitService.cloneRepository(gitRepoUrl, "main");		
+		gitService.commitFiles("Test Commit", "main");
 	}
 
 }
